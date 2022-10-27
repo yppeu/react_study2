e0e99c0e21fbe2f6629d14ae6215eb4d27b5403d
yppeu <yppeu@naver.com>
2022-10-27 15:47:52 +0900

    1027

--- src/App.js
+++ src/App.js
@@ -8,57 +8,46 @@ function App() {
   const post = '강남 우동 맛집';
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
   const [따봉,따봉변경] = useState(0);
-
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
         </div>
-        {/* <div className="list">
-            <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>❤️</span>{따봉}</h4>
-            <p>10월 26일 발행</p>
-            <button onClick={()=>{
-              let copy = [...글제목];
-              copy[0] = '여자 코트 추천';
-              // 글제목[0] = '여자코트 추천';
-              글제목변경(copy)
-              }}>on</button>
-        </div>
-        <div className="list">
-            <h4>{글제목[1]}</h4>
-            <p>10월 26일 발행</p>
-        </div>
-        <div className="list">
-            <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}</h4>
-            <p>10월 26일 발행</p>
-        </div> */}
         {
-      글제목.map(function (a,i) {
+      글제목.map(function (data,i) {
         return (
           <div className="list" key={i}>
-            <h4 onClick={()=>{setModal(!modal)}}>{글제목[i]}<span onClick={()=>{따봉변경(따봉+1)}}>❤️</span>{따봉}</h4>
+            <h4 onClick={()=>{setModal(!modal); setTitle(i)}}>{글제목[i]}
+              <span onClick={()=>{따봉변경(따봉+1)}}>❤️</span>{따봉}</h4>
             <p>10월 26일 발행</p>
           </div>
         );
       })
       }
         {
-          modal === true ? <Modal></Modal> : <></>
+          modal === true ? <Modal color={'pink'} title={title} 글제목={글제목} 글제목변경={글제목변경} /> : <></>
         }
-        
       </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
-    <div className="modal">
+    <div className="modal" style={{background:props.color}}>
     <div>
-      <h4>{글제목[0]}</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={()=>{
+        let 글제목2 = [...props.글제목];
+        const array = ['여자 코트 추천','홍대 우동맛집','자바독학'];
+        글제목2[props.title] = array[props.title] 
+        props.글제목변경(글제목2);
+      }}>글수정</button>
     </div>
     </div>
   )
